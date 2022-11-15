76661c8542684b3764aa497e4798c58ae8e0459e
lucas-ribard <lucas.ribard@laplateforme.io>
2022-11-15 14:04:38 +0100

    Job01.2

--- jour01/job01/index.php
+++ jour01/job01/index.php
@@ -1,10 +1,11 @@
 <?php
+
     $str1 = 'Vive';
     $str2 = 'LaPlateforme';
     $str3 = '!';
-    echo "$str1 $str2 $str3";
-
-
+    echo "$str1 $str2 $str3<br/>";
+    
     $val = 6;
+    echo "$val<br/>";
     echo $val+4;
 ?>
