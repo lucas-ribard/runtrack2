2b6b1e78d37dbf3e9c8435685cfe7bd5b5fe498f
lucas-ribard <lucas.ribard@laplateforme.io>
2022-11-16 11:23:29 +0100

    jour2 job02

---
+++ Jour02/job02/index.php
@@ -0,0 +1,12 @@
+<?php
+//compte de 1 a 1337
+for ($i = 1; $i <= 1337; $i++) {
+    //si le nombre est 44
+    if ($i === 26 or $i === 37 or $i === 88 or $i === 1111 or $i === 3233){
+        echo "<br>";
+    }
+    //sinon (pas 42) ecrit le chiffre courrant
+    else{echo "$i<br>";}
+    }
+
+?>
