f36210783ff2ac6901e36e32f2e1ee013d2c2604
lucas-ribard <lucas.ribard@laplateforme.io>
2022-11-16 15:48:45 +0100

    jour3 job02

--- jour03/job01/index.php
+++ jour03/job01/index.php
@@ -2,6 +2,7 @@
 //le tableau
 $tableau = array (200, 204, 173, 98, 171, 404, 459);
 
+//compte nombre d'éléments dans le tableau
 //deja le faire marcher et ensuite compliquer en enlevant les fonctions systemes                                                                                                                                                                                                            on s'embete pour rien encore
 $nbpos=count($tableau);
 
---
+++ jour03/job02/index.php
@@ -0,0 +1,16 @@
+<?php
+
+$str = "Tous ces instants seront perdus dans le temps comme les larmes sous la pluie.";
+
+echo "$str<br>";
+//tant que la phrase ne dépasse pas 100 mots tout ira bien  (on peux toujours monter la quantité)
+for ($i = 0; $i <= 100; $i++) {
+    //un chiffre sur deux est impaire
+    //donc chaque nombre paire fera une lettre sur deux
+    if($i % 2 ==0){
+        $nombre = $str[$i];
+        echo $nombre;
+    }
+}
+
+?>
