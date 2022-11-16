5c1fa54ecfb6be10ce24b777da4997f7dc4ba454
lucas-ribard <lucas.ribard@laplateforme.io>
2022-11-16 14:09:32 +0100

    jour2 job05

--- Jour02/job05/index.php
+++ Jour02/job05/index.php
@@ -1,21 +1,26 @@
-<?php
-//compte de 1 a 1000
-for ($i = 1; $i <= 1000; $i++) {
-    echo "compteur $i<br>";
-    //test de diviser $i par tout les nombres entre 0 et $i
-    for ($x = 1; $x <= $i; $x++) {
-        
-        if ($i % $x == 0 ){
-            $premier=true;
-        }
-        else{
-            $premier=false;
-        }
-    }
-    echo "test premier $i<br>";
-    if($premier=true){
-        echo "$i<br>";
-    }
 
+<?php
+//compte de 2 a 1000    (1 ne compte pas vraiment)
+for ($i = 2; $i <= 1000; $i++) {
+    //reset etat de $premier
+    $premier=true;
+    //compteur x commence a 2 calcule pas avec 1 est max $1-1    car nombre premier divisable que par 1 et lui meme
+    for ($x = 2; $x <= $i-1; $x++) {
+
+
+        $test=$i%$x;
+        //apres chaque test si le nombre est divisible (entre 1 est lui meme) , il n'est donc pas premier 
+        if ($test===0){
+            //le nombre n'est pas premier
+            $premier=false;
+            //on met fin a la boucle
+            break;
+        }
+
+}
+//si le nombre est premier on l'écrit
+if ($premier==true){
+    echo "$i est premier <br>";
+}
 }
 ?>
