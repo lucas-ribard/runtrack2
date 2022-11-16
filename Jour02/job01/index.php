c8800d30390b8401c87784a4bb06246e0d1d2661
lucas-ribard <lucas.ribard@laplateforme.io>
2022-11-16 11:16:50 +0100

    job01

---
+++ Jour02/job01/index.php
@@ -0,0 +1,15 @@
+<?php
+
+for ($i = 1; $i <= 1337; $i++) {
+    if ($i === 42){
+        echo "<b><u>";
+        echo 42;
+        echo "</b></u><br>";
+
+    }
+    else{echo "$i<br>";}
+    }
+
+?>
+
+
