f8ebae7dcbc0bf93ab969be5b3a5fcbe15c4c7a2
lucas-ribard <lucas.ribard@laplateforme.io>
2022-11-16 11:52:53 +0100

    jour2 job04

--- Jour02/job04/index.php
+++ Jour02/job04/index.php
@@ -1,7 +1,7 @@
 <?php
 //compte de 1 a 100
 for ($i = 1; $i <= 100; $i++) {
-    //si le nombre est divisble par 3
+    //si le nombre est divisble par 3 mais pas par 5
     if ($i % 3 == 0 xor $i % 5 == 0){
 
         echo "Fizz<br>";
@@ -9,12 +9,12 @@ for ($i = 1; $i <= 100; $i++) {
     
     
 
-     //si le nombre est divisble par 5
+     //si le nombre est divisble par 5  mais pas par 3
     elseif ($i % 5 == 0 xor $i % 3 == 0){
 
         echo "Buzz<br>";
     }
-    
+    //si le nombre est divisble par 5 et par 3
     elseif ($i % 3 == 0 and $i % 5 == 0){
         echo "FizzBuzz<br>";
     }
