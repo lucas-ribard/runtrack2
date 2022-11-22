b33494e619d4d93f33964734bfbab4be30ce4bab
lucas-ribard <lucas.ribard@laplateforme.io>
2022-11-22 16:24:45 +0100

    Fix Jour07 Job06 Fini

--- jour07/job06/index.php
+++ jour07/job06/index.php
@@ -16,31 +16,29 @@ function leetSpeak($str){
     $leet = "48CD3F6HIJK1MNOPQR57UVWXYZ";
     $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJK1MNOPQRSTUVWXYZ';
     
-    for ($i = 0; $i <= 2000; $i++) {
-        //recup la lettre
-        $lettre = $str[$i];
-
-        //si lettre dans les 26 premieres lettres de l'alphabet
-        for ($x = 0; $x <= 54; $x++) {
-            $test=$alphabet[$x];
-            if ($lettre == $test){
-                //si maj
-                if ($x >= 26 ){
-                    $tempx=($x-26);
-                }
-                //si min
-                elseif ($x <= 25){
-                    $tempx=$x;
-                }
-               //ecrit dans str
-                $str[$i]=$leet[$tempx];
-            }
-        } 
-        //pourquoi il marche ici
-        echo "<br> $str";
-    }
-    //pourquoi il marche PAS ici
-    echo "<br>Miracle !<br> $str";
+    for ($i = 0; $i <= 50; $i++) {
+            //recup la lettre
+            $lettre = $str[$i];
+            // [SOLUCE fix j7j6]   fait les boucles tant que $str[$i] a une valeur (sinon il reste coincé dans la boucle d'au dessus)
+            if (isset($str[$i])){
+                //si lettre dans les 26 premieres lettres de l'alphabet
+                for ($x = 0; $x <= 54; $x++) {
+                    $test=$alphabet[$x];
+                    if ($lettre == $test){
+                        //si maj
+                        if ($x >= 26 ){
+                            $tempx=($x-26);
+                        }
+                        //si min
+                        elseif ($x <= 25){
+                            $tempx=$x;
+                        }
+                    //ecrit dans str
+                        $str[$i]=$leet[$tempx];
+                    }
+                } 
+        }
+}
     return($str);
 }
 
