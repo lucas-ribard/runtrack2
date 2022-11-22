726ed5381741c5acb308fb6686c5d96e8ead7784
lucas-ribard <lucas.ribard@laplateforme.io>
2022-11-22 17:02:19 +0100

    jour7 job7

---
+++ jour07/job07/index.php
@@ -0,0 +1,68 @@
+<!-- Formulaire -->
+<form action="" method="post">
+    <label for="str">Votre texte :</label><br>
+    <input type="text" name="str" id="str"><br>
+    <br>
+    <label for="option">Background Color:</label>
+    <select name="option" id="option">
+        <option valeur="gras">Gras</option>
+        <option valeur="cesar">Cesar</option>
+        <option valeur="plateforme">Plateforme</option>
+    </select><br>
+    <br>
+    <input type="submit" value="envoyer"><br>
+   
+</form>
+
+
+<?php 
+
+    //init values
+
+    $str=$_POST["str"];
+    $option=$_POST["option"];
+
+    echo"$str<br>";
+    echo"$option<br>";
+
+    $alphabet="abcdefghijklmnopqrstuvwxyz";
+    $ALPHABET="ABCDEFGHIJKLMNOPQRTSUVWXYZ";
+
+    //fonctions 
+
+    function gras($str,$ALPHABET){
+        //met en gras les mots qui comencent par une MAJ
+        /*
+        if dans $ALPHABET mettre en gras jusqu'a " "
+        */
+    }
+    function cesar($str,$alphabet,$ALPHABET){
+        //fait un decalage de lettre A décalage de 2 = C
+        //décalage vaut 2 par default , rajouter un imput ? plus tard
+        /*
+        boucle $i
+        alphabet[$i]
+        lettre = alphabet[$i+$déclage]
+        */
+    }
+    function plateforme($str){
+        //si finit par me ajoute "_"
+    }
+
+    //code
+
+    if ($option==="gras"){
+        $str=gras($str);
+
+    } 
+    if ($option==="cesar"){
+        $str=cesar($str);
+
+    }
+    if ($option==="plateforme"){
+        $str=plateforme($str);
+
+    }
+?>
+
+		
