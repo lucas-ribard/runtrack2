c9bd3131b752be23592c78d4e7f9888aec980cbd
lucas-ribard <lucas.ribard@laplateforme.io>
2022-11-23 12:16:25 +0100

    jour08 job03

---
+++ jour08/job03/index.php
@@ -0,0 +1,26 @@
+<form action="" method="post">
+    
+        <label for="prenom">Votre Prenom :</label><br>
+        <input type="text" name="prenom" id="prenom">              
+        <input type="submit" value="envoyer"><br>
+        <input type = 'submit' name='reset' value = 'reset'>
+    
+</form>
+
+
+<?php
+
+session_start();
+//ajoute le prenom dans l'array
+array_push($_SESSION['lstprenom'],$_POST['prenom']);
+
+//si reset
+if (isset ($_POST['reset'])){
+    $_SESSION['lstprenom']=array();
+}
+
+foreach ($_SESSION['lstprenom'] as  $value) {
+    echo "&ensp;&bull;$value<br>";
+}
+
+?>
